66833ef3eba2eaf4702240cc5684aab11646ebbb
sanket pathak <sanketpathak64@gmail.com>
2020-05-04 00:10:30 +0530

    added empty file for testing

---
+++ Data Structures/Stack/stack.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+/***********************************
+contributed by sanketpathak64
+Implemented following functions for stack
+
+***********************************/
